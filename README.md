3d1afe85e3567f25d535ba16aca177daf90347eb
Ferran Borreguero <ferran.borreguero@gmail.com>
2024-07-30 09:13:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## `Flashbots-toolchain` Github action
+
+This Github action installs different tools from the Flashbots toolchain.
+
+### Example workflow
+
+```yaml
+on: [push]
+
+jobs:
+  hello_world_job:
+    runs-on: ubuntu-latest
+    name: Suapp
+    steps:
+      - name: Install suave-geth
+        uses: flashbots/flashbots-toolchain@v0.1
+        with:
+          suave-geth: latest
+      - name: Which suave-geth
+        run: suave-geth version
+```
+
+### Inputs
+
+| **Name**             | **Required** | **Description**                                | **Type** |
+| -------------------- | ------------ | ---------------------------------------------- | -------- |
+| `suave-geth`         | No           | Version to install, e.g. `latest` or `v0.1.0`. | string   |
+| `builder-playground` | No           | Version to install, e.g. `latest` or `v0.1.0`. | string   |
---
+++ src/index.js
@@ -0,0 +1,46 @@
+const core = require("@actions/core");
+const toolCache = require("@actions/tool-cache");
+const path = require("path");
+const axios = require("axios");
+
+let repos = ["suave-geth", "builder-playground"];
+
+async function main() {
+  for (let repo of repos) {
+    try {
+      await tryDownloadRelease(repo);
+    } catch (error) {
+      core.setFailed(`Failed to download ${repo}: ${error}`);
+    }
+  }
+}
+
+async function tryDownloadRelease(repo) {
+  let version = core.getInput(repo);
+  if (version == "") {
+    return
+  } else if (version == "latest") {
+    core.info(`Fetching latest release for ${repo}`);
+    const response = await axios.get(`https://api.github.com/repos/flashbots/${repo}/releases/latest`);
+    version = response.data.tag_name;
+  }
+
+  const url = `https://github.com/flashbots/${repo}/releases/download/${version}/${repo}_${version}_linux_amd64.zip`;
+
+  core.info(`Downloading ${repo} from: ${url}`);
+  const pathToArchive = await toolCache.downloadTool(url);
+
+  // Extract the archive onto host runner
+  core.debug(`Extracting ${pathToArchive}`);
+  const extract = url.endsWith(".zip") ? toolCache.extractZip : toolCache.extractTar;
+  const pathToCLI = await extract(pathToArchive);
+
+  // Expose the tool
+  core.addPath(path.join(pathToCLI, "."));
+}
+
+module.exports = main;
+
+if (require.main === module) {
+  main();
+}
